a9038a6995a6dfa86e8eb1809ac049ad05bd36b6
Lohanna <lohannabt@gmail.com>
2026-05-07 11:27:35 -0300

    axios

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://blogpessoal-nestjs.onrender.com"
+    baseURL: "https://blogpessoal-nest-jqt5.onrender.com/"
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
