17c802ed470a443a40d646685d863f1278dc797c
Lohanna <lohannabt@gmail.com>
2026-05-06 21:51:29 -0300

    dom

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,13 +10,19 @@ function Footer() {
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
+                            Blog Pessoal Lohanna | Copyright: {data}
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <FacebookLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/lohannab" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/lohannabr" target="_blank">
+                            <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/lohannabr" target="_blank">
+                            <FacebookLogoIcon size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
